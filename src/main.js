17c0ecb1e2cfd0714469a571a40ba581ab76e872
taxiisnottaxi <2417219614@qq.com>
2020-08-13 21:43:19 +0800

    完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -11,6 +11,11 @@ import "./assets/css/global.css"
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 最后必须return config
+  return config
+})
 // 将axios这个对象暴露给Vue
 Vue.prototype.$http = axios
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
+/* eslint-disable */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
 
 Vue.use(VueRouter)
 
@@ -9,7 +12,12 @@ const router = new VueRouter({
   routes: [
     { path: '/', redirect: '/login' },
     { path: '/login', component: Login },
-    { path: '/home', component: Home }
+    { 
+      path: '/home',
+      component: Home,
+      redirect: 'welcome',
+      children: [{ path: '/welcome', component: Welcome },{ path: '/users', component: Users }]
+    }
   ]
 })
 
@@ -24,6 +32,7 @@ router.beforeEach((to, from, next) => {
   if (to.path === '/login') return next()
   // 获取token
   const tokenStr = window.sessionStorage.getItem('token')
+  console.log(tokenStr)
   if (!tokenStr) return next('/login')
   next()
 })
