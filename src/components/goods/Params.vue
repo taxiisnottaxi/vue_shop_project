ea11e5783055e7b6bea0805c1093b6f827dd32ee
taxiisnottaxi <2417219614@qq.com>
2020-08-21 16:07:00 +0800

    项目完善

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -267,9 +267,9 @@ export default {
             })
         },
         // 点击按钮，展示修改对话框
-        async showEditDialog(attr_id) {
+        async showEditDialog(attrId) {
             // 查询当前参数的信息
-            const {data: res} = await this.$http.get(`categories/${this.cateId}/attributes/${attr_id}`, {
+            const {data: res} = await this.$http.get(`categories/${this.cateId}/attributes/${attrId}`, {
                 params: {
                     attr_sel: this.activeName
                 }
@@ -301,7 +301,7 @@ export default {
             })
         },
         // 根据id删除对应的参数项
-        async removeEditDialog(attr_id) {
+        async removeEditDialog(attrId) {
             const confirmResult = await this.$confirm('此操作将永久删除该参数, 是否继续?', '提示', {
                 confirmButtonText: '确定',
                 cancelButtonText: '取消',
@@ -314,7 +314,7 @@ export default {
             }
 
             // 删除业务逻辑
-            const {data: res} = await this.$http.delete(`categories/${this.cateId}/attributes/${attr_id}`)
+            const {data: res} = await this.$http.delete(`categories/${this.cateId}/attributes/${attrId}`)
             
             if(res.meta.status !== 200) {
                 return this.$message.error('删除参数失败！')
--- src/main-prod.js
+++ src/main-prod.js
@@ -2,7 +2,7 @@
 import Vue from "vue"
 import App from "./App.vue"
 import router from "./router"
-import "./plugins/element.js"
+// import "./plugins/element.js"
 // 导入字体图标
 import './assets/fonts/iconfont.css'
 // 导入全局样式表
@@ -10,17 +10,24 @@ import "./assets/css/global.css"
 import TreeTable from 'vue-table-with-tree-grid'
 // 导入富文本编辑器
 import VueQuillEditor from 'vue-quill-editor'
-// 导入富文本编辑器对应的样式
-import 'quill/dist/quill.core.css' // import styles
-import 'quill/dist/quill.snow.css' // for snow theme
-import 'quill/dist/quill.bubble.css' // for bubble theme
+
+// 导入 NProgress 包对应的JS和CSS
+import NProgress from 'nprogress'
 
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 在request拦截器中展示进度条，NProgress.start()
 axios.interceptors.request.use(config => {
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
   // 最后必须return config
+  return config
+})
+
+// 在response辣鸡恶气中,隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
   return config
 })
 // 将axios这个对象暴露给Vue
